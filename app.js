7911a69963336d8de130544efde13d4d56395ab0
Sharyu Deshmukh <33131470+sharyudeshmukh@users.noreply.github.com>
2024-07-17 17:52:52 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log('Is this working?');
+
+let viz;
+
+//Add Share Link to Tableau Public in here
+const url = "https://public.tableau.com/shared/KM8X46QS6?:display_count=n&:origin=viz_share_link";
+
+const vizContainer = document.getElementById('vizContainer');
+const options = {
+    hideTabs: true,
+    height: 1000,
+    width: 1200,
+    onFirstInteraction: function() {
+        workbook = viz.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+
+//create a function to generate the viz element
+function initViz() {
+    console.log('Executing the initViz function!');
+    viz = new tableau.Viz(vizContainer, url, options);
+}
+
+// run the initViz function when the page loads
+document.addEventListener("DOMContentLoaded", initViz);
