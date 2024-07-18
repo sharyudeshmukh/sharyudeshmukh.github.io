0aa516173235aad5b17272bfb03d04cbefe99ea1
Sharyu Deshmukh <33131470+sharyudeshmukh@users.noreply.github.com>
2024-07-17 20:07:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,34 +4,33 @@ let viz;
 let workbook;
 let activeSheet;
 
-// Add Share Link to Tableau Public here
-const url = "https://public.tableau.com/views/Caloriesupplybyfoodgroup/Dashboard1";
-
-const vizContainer = document.getElementById('vizContainer');
-const options = {
-    hideTabs: true,
-    height: 1000,
-    width: 1000,
-    onFirstInteraction: function() {
-        try {
-            workbook = viz.getWorkbook();
-            activeSheet = workbook.getActiveSheet();
-            console.log("My dashboard is interactive");
-        } catch (error) {
-            console.error("Error during first interaction:", error);
-        }
-    }
-};
-
 // Create a function to generate the viz element
-function initViz() {
+function loadViz(containerId, url) {
     try {
-        console.log('Executing the initViz function!');
+        const vizContainer = document.getElementById(containerId);
+        const options = {
+            hideTabs: true,
+            height: 600, // Adjusted height to match the container
+            width: '100%', // Adjusted width to match the container
+            onFirstInteraction: function() {
+                try {
+                    workbook = viz.getWorkbook();
+                    activeSheet = workbook.getActiveSheet();
+                    console.log("My dashboard is interactive");
+                } catch (error) {
+                    console.error("Error during first interaction:", error);
+                }
+            }
+        };
+
+        console.log('Executing the loadViz function for ' + containerId + '!');
         viz = new tableau.Viz(vizContainer, url, options);
     } catch (error) {
         console.error("Error initializing Tableau viz:", error);
     }
 }
 
-// Run the initViz function when the page loads
-document.addEventListener("DOMContentLoaded", initViz);
+// Initial call to load the first dashboard when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    loadViz('vizContainer1', 'https://public.tableau.com/views/Caloriesupplybyfoodgroup/Dashboard1');
+});
